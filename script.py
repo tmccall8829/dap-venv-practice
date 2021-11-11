279e1730a0e8628ec3cb60e69d9be9903b27931c
Tom McCall <thomas.e.mccall@gmail.com>
2021-11-11 14:42:38 -0600

    added script

--- script.py
+++ script.py
@@ -1,2 +1,6 @@
 import pandas as pd
-import geopandas as gpd
+
+if pd.__version__ != "1.3.1":
+    print(f"You didn't load the version! It should be 1.3.1; you loaded {pd.__version__}")
+else:
+    print(f"Good job! You loaded version {pd.__version__}")
